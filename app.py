ed250648515bd083b59e4928e39427f6f54f4752
noelkhalaf <noel_khalaf@hotmail.com>
2023-03-12 18:38:15 -0400

    Added insert statements in sql and execute query

--- app.py
+++ app.py
@@ -7,10 +7,15 @@ db = mysql.connector.connect(
     host='localhost',
     user='root',
     passwd='root',
-    database='e-hotels'
+    database='e_hotels'
 )
 
 cursor = db.cursor()
+
+# cursor.execute("CREATE DATABASE IF NOT EXISTS e_hotels")
+
+with open('schema.sql', 'r') as f:
+    db._execute_query(f.read())
 
 @app.route('/', methods=['GET'])
 def index():
@@ -18,11 +23,11 @@ def index():
     
 @app.route('/employee/', methods=['GET'])
 def employee_page():
-    return render_template('employee.html')
+    return render_template('index.html')
 
 @app.route('/customer/', methods=['GET'])
 def customer_page():
-    return render_template('customer.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(port=7777)
