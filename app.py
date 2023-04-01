fb63c39801ed842f8a3c7d2650f3190df989062c
Youssef Elfaramawy <yelfa029@uottawa.ca>
2023-04-01 18:29:32 -0400

    Added new html with css file and backend routes

--- app.py
+++ app.py
@@ -17,17 +17,67 @@ cursor = db.cursor()
 with open('schema.sql', 'r') as f:
     db._execute_query(f.read())
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
     return render_template('index.html')
     
-@app.route('/employee/', methods=['GET'])
-def employee_page():
-    return render_template('index.html')
+@app.route('/employee')
+def employee():
+    return render_template('employee.html')
 
-@app.route('/customer/', methods=['GET'])
-def customer_page():
-    return render_template('index.html')
+@app.route('/customer')
+def customer():
+    return render_template('customer.html')
+
+@app.route('/customerSignIn', methods=['GET', 'POST'])
+def customerSignIn():
+    msg = ''
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        cursor.execute('SELECT * FROM CUSTOMER WHERE username = % s AND password = % s', (username, password, ))
+        customer = cursor.fetchone()
+        if customer:
+            msg = 'Login success !'
+            return render_template('index.html', msg = msg)
+        else:
+            msg = 'Incorrect username or password'
+    return render_template('customer_signin.html', msg = msg)
+
+@app.route('/employeeSignIn', methods=['GET', 'POST'])
+def emplyeeSignIn():
+    msg = ''
+    if request.method == 'POST':
+        employee_id = request.form['employee_id']
+        fname = request.form['fname']
+        cursor.execute('SELECT * FROM EMPLOYEE WHERE employee_id = % s AND first_name = % s', (employee_id, fname, ))
+        employee = cursor.fetchone()
+        if employee:
+            msg = 'Login success !'
+            return render_template('index.html', msg = msg)
+        else:
+            msg = 'Incorrect id or first name'
+    return render_template('employee_signin.html', msg = msg)
+
+@app.route('/customerSignUp', methods=['GET', 'POST'])
+def customerSignUp():
+    msg = ''
+    if request.method == 'POST':
+        username = request.form['username']
+        sxn = request.form['sxn']
+        fname = request.form['fname']
+        lname = request.form['lname']
+        address = request.form['address']
+        password = request.form['password']
+        cursor.execute('SELECT * FROM CUSTOMER WHERE username = % s', (username, ))
+        customer = cursor.fetchone()
+        if not customer:
+            cursor.execute('INSERT INTO CUSTOMER VALUES (NULL, %s, %s, % s, % s, % s, % s, CURDATE())', (username, password, sxn, fname, lname, address, ))
+            db.commit()
+            msg = 'Customer created successfully !'
+        else:
+            msg = 'Customer already exists'
+    return render_template('customer_signup.html', msg = msg)
 
 if __name__ == '__main__':
-    app.run(port=7777)
+    app.run(debug=True, port=7777)
